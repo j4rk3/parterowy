35e5050d6685b7f85a4c3ba911dee39d748770af
j4rk3 <elosiema.elosiema@gmail.com>
2026-07-06 17:27:19 +0200

    email kontakt fix

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -19,7 +19,7 @@ export default function ContactSection() {
     if (!rodo) return alert('Proszę zaakceptować klauzulę RODO.')
     setStatus('sending')
     try {
-      const res = await fetch('https://formspree.io/f/TWOJ_KOD', {
+      const res = await fetch('https://formspree.io/f/mjgqkqpw', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
